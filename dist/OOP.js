50c9ed4da1b409c46dc62d3739247a7312da1e20
sohelkhan <mdsohelkhan418126@gmail.com>
2026-08-17 13:24:08 +0600

    Refactor OOP structure: replace Person class with Student class and add BankAccount class with balance display functionality

--- dist/OOP.js
+++ dist/OOP.js
@@ -1,22 +1,54 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // classes oop
-class Person {
+// class Person {
+//   name: string;
+//   age: number;
+Object.defineProperty(exports, "__esModule", { value: true });
+//   constructor() {
+//     ((this.name = 'sohel'), (this.age = 20));
+//   }
+//   greet() {
+//     console.log(`this name ${this.name}`);
+//   }
+// }
+// const user = new Person();
+// ((user.name = 'sohel'), (user.age = 43));
+// user.greet();
+// console.log(user);
+// const user2 = new Person();
+// ((user.name = 'rana'), (user.age = 23));
+// user.greet();
+// console.log(user2);
+// constructor
+class Student {
     name;
     age;
-    constructor() {
-        ((this.name = 'sohel'), (this.age = 20));
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
     }
-    greet() {
-        console.log(`this name ${this.name}`);
+    add() {
+        console.log(`hi ${this.name} she is age ${this.age}`);
     }
 }
-const user = new Person();
-((user.name = 'sohel'), (user.age = 43));
-user.greet();
-console.log(user);
-const user2 = new Person();
-((user.name = 'rana'), (user.age = 23));
-user.greet();
-console.log(user2);
+const user = new Student('sohel', 34);
+user.add();
+const user2 = new Student('Rana', 22);
+console.log(user2.name);
+// bank account
+class BankAccount {
+    accountName;
+    balance;
+    AccountType;
+    constructor(name, amount) {
+        this.accountName = name;
+        this.balance = amount;
+        this.AccountType = "Savings";
+    }
+    showBalance() {
+        console.log(` hi balance is ${this.balance}`);
+    }
+}
+const account = new BankAccount('sohel mia', 1000);
+account.showBalance();
 //# sourceMappingURL=OOP.js.map
--- src/OOP.ts
+++ src/OOP.ts
@@ -1,21 +1,65 @@
 // classes oop
-class Person {
+// class Person {
+//   name: string;
+//   age: number;
+
+//   constructor() {
+//     ((this.name = 'sohel'), (this.age = 20));
+//   }
+
+//   greet() {
+//     console.log(`this name ${this.name}`);
+//   }
+// }
+// const user = new Person();
+// ((user.name = 'sohel'), (user.age = 43));
+// user.greet();
+// console.log(user);
+// const user2 = new Person();
+// ((user.name = 'rana'), (user.age = 23));
+// user.greet();
+// console.log(user2);
+
+// constructor
+
+class Student {
   name: string;
   age: number;
 
-  constructor() {
-    ((this.name = 'sohel'), (this.age = 20));
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
   }
-
-  greet() {
-    console.log(`this name ${this.name}`);
+  add() {
+    console.log(`hi ${this.name} she is age ${this.age}`);
   }
 }
-const user = new Person();
-((user.name = 'sohel'), (user.age = 43));
-user.greet();
-console.log(user);
-const user2 = new Person();
-((user.name = 'rana'), (user.age = 23));
-user.greet();
-console.log(user2);
+const user = new Student('sohel', 34);
+user.add();
+const user2 = new Student('Rana', 22);
+console.log(user2.name);
+
+
+// bank account
+
+class BankAccount{
+  public accountName: string,
+  private balance:number
+protected AccountType:string
+
+  constructor(name:string,amount:number) {
+    this.accountName = name;
+    this.balance = amount;
+  this.AccountType="Savings"
+  
+  }
+ public showBalance() {
+  
+  console.log(` hi balance is ${ this.balance}`);
+  
+  }
+
+}
+const account = new BankAccount('sohel mia', 1000)
+account.showBalance();
+
